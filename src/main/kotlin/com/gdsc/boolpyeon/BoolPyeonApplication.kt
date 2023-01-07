ecb420e2eaeb33507646bc4ea81ee4f1d36d60c9
kiryanchi <kiryanchi@naver.com>
2023-01-07 14:42:20 +0900

    init commit

---
+++ src/main/kotlin/com/gdsc/boolpyeon/BoolPyeonApplication.kt
@@ -0,0 +1,11 @@
+package com.gdsc.boolpyeon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoolPyeonApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoolPyeonApplication>(*args)
+}
---
+++ src/test/kotlin/com/gdsc/boolpyeon/BoolPyeonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gdsc.boolpyeon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoolPyeonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
