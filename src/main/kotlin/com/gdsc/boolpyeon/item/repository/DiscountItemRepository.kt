d16e381ef9408ca409e8d83d65aa1999fae36cb5
chaejm55 <chaejm55@gmail.com>
2023-01-29 23:35:07 +0900

    item api 수정 및 경로 변경

--- src/main/kotlin/com/gdsc/boolpyeon/item/repository/DiscountItemRepository.kt
+++ src/main/kotlin/com/gdsc/boolpyeon/item/repository/DiscountItemRepository.kt
@@ -3,6 +3,7 @@ package com.gdsc.boolpyeon.item.repository
 import com.gdsc.boolpyeon.item.entity.DiscountItem
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.ListResourceBundle
 
 @Repository
 interface DiscountItemRepository : JpaRepository<DiscountItem, Long> {
